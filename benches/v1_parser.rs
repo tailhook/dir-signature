81d696f6011c0d5dcc83b8b870259c9f08aa8545
Alexander Koval <kovalidis@gmail.com>
2017-05-18 23:03:52 +0300

    Implement Iterator for MergedEntriesIterator

--- benches/v1_parser.rs
+++ benches/v1_parser.rs
@@ -10,33 +10,20 @@ use test::Bencher;
 
 extern crate dir_signature;
 use dir_signature::v1::{Entry, Parser};
+use dir_signature::v1::merge::FileMergeBuilder;
 
 #[bench]
 fn bench_parser_iterator_ubuntu(bencher: &mut Bencher) {
-    let ubuntu_idx_path = Path::new("benches/ubuntu-xenial.v1.idx");
-    if !ubuntu_idx_path.exists() {
-        println!("");
-        println!("To generate direcotry signature run: vagga gen-test-data");
-        process::exit(1);
-    }
+    let sig_path = get_ubuntu_signature_path();
+    let (num_dirs, num_files, num_links) = warmup_signature_file(sig_path);
+    println!("");
+    println!("Signature file contains:");
+    println!("{} directories", num_dirs);
+    println!("{} files", num_files);
+    println!("{} links", num_links);
 
-    let mut num_dirs = 0;
-    let mut num_files = 0;
-    let mut num_links = 0;
-    let idx_file = File::open(ubuntu_idx_path).unwrap();
-    let reader = BufReader::new(idx_file);
-    let mut signature_parser = Parser::new(reader).unwrap();
-    for entry in signature_parser.iter() {
-        match entry.unwrap() {
-            Entry::Dir(_) => num_dirs += 1,
-            Entry::File{..} => num_files += 1,
-            Entry::Link(..) => num_links += 1,
-        }
-    }
-
-    let mut num_iters = 0;
     bencher.iter(|| {
-        let idx_file = File::open(ubuntu_idx_path).unwrap();
+        let idx_file = File::open(sig_path).unwrap();
         let reader = BufReader::new(idx_file);
         let mut signature_parser = Parser::new(reader).unwrap();
         for entry in signature_parser.iter() {
@@ -46,12 +33,58 @@ fn bench_parser_iterator_ubuntu(bencher: &mut Bencher) {
                 Entry::Link(..) => {},
             }
         }
-        num_iters += 1;
     });
-    println!("");
-    println!("{} iterations", num_iters);
-    println!("Signature file contains:");
-    println!("{} directories", num_dirs);
-    println!("{} files", num_files);
-    println!("{} links", num_links);
+}
+
+#[bench]
+fn bench_merged_iterator(bencher: &mut Bencher) {
+    let sig_path = get_ubuntu_signature_path();
+    warmup_signature_file(sig_path);
+
+    bencher.iter(|| {
+        let mut merge_builder = FileMergeBuilder::new();
+        merge_builder.add("/a", sig_path);
+        merge_builder.add("/b", sig_path);
+        merge_builder.add("/c", sig_path);
+        let mut merged = merge_builder.finalize().unwrap();
+        let merged_iter = merged.iter();
+        for entries in merged_iter {
+            assert_eq!(entries.len(), 3);
+            for (_, entry) in entries {
+                let entry = entry.unwrap();
+                match entry {
+                    Entry::Dir(_) => {},
+                    Entry::File{..} => {},
+                    Entry::Link(..) => {},
+                }
+            }
+        }
+    });
+}
+
+fn get_ubuntu_signature_path<'a>() -> &'a Path {
+    let ubuntu_idx_path = Path::new("benches/ubuntu-xenial.v1.idx");
+    if !ubuntu_idx_path.exists() {
+        println!("");
+        println!("To generate direcotry signature run: vagga gen-test-data");
+        process::exit(1);
+    }
+    ubuntu_idx_path
+}
+
+fn warmup_signature_file(path: &Path) -> (usize, usize, usize) {
+    let mut num_dirs = 0;
+    let mut num_files = 0;
+    let mut num_links = 0;
+    let idx_file = File::open(path).unwrap();
+    let reader = BufReader::new(idx_file);
+    let mut signature_parser = Parser::new(reader).unwrap();
+    for entry in signature_parser.iter() {
+        match entry.unwrap() {
+            Entry::Dir(_) => num_dirs += 1,
+            Entry::File{..} => num_files += 1,
+            Entry::Link(..) => num_links += 1,
+        }
+    }
+    (num_dirs, num_files, num_links)
 }
--- tests/merge.rs
+++ tests/merge.rs
@@ -104,3 +104,122 @@ fn test_merge_different_block_sizes() {
             Err(MergeError::BlockSizesMismatch(ref sizes))
             if sizes == &vec!(32768, 65536)));
 }
+
+#[test]
+fn test_merge_iter() {
+    let content1 = b"\
+DIRSIGNATURE.v1 sha512/256 block_size=32768
+/
+/a
+  .hidden f 28394 24f72d3a930b5f7933ddd91a5c7cb7ba09a093f936a04bf6486c8b1763c59819
+/b
+  empty.txt f 0
+c23f2579827456818fc855c458d1ad7339d144b57ee247a6628e4fc8e39958bb
+";
+    let content2 = b"\
+DIRSIGNATURE.v1 sha512/256 block_size=32768
+/
+  1 f 0
+  hello.txt f 5 339d14455c458d1ad7b57ee247a6628e4fc8e39958bbc23f2579827456818fc8
+/a
+  .hidden f 28394 24f72d3a930b5f7933ddd91a5c7cb7ba09a093f936a04bf6486c8b1763c59819
+/c
+  empty.txt f 0
+c23f2579827456818fc855c458d1ad7339d144b57ee247a6628e4fc8e39958bb
+";
+    let parsers = vec!(
+        (
+            PathBuf::from("/111"),
+            Parser::new(BufReader::new(Cursor::new(&content1[..]))).unwrap()
+        ),
+        (
+            PathBuf::from("/222"),
+            Parser::new(BufReader::new(Cursor::new(&content2[..]))).unwrap()
+        ),
+    );
+
+    let mut merger = MergedSignatures::new(parsers).unwrap();
+    let mut merged_iter = merger.iter();
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 2);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::Dir(ref path)))
+                     if base_path == Path::new("/111") &&
+                     path == Path::new("/")),
+            "Was: {:?}", entry);
+    let ref entry = entries[1];
+    assert!(matches!(entry, &(base_path, Ok(Entry::Dir(ref path)))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/")),
+            "Was: {:?}", entry);
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 1);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::File{ref path, size, ..}))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/1") && size == 0),
+            "Was: {:?}", entry);
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 1);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::File{ref path, size, ..}))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/hello.txt") && size == 5),
+            "Was: {:?}", entry);
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 2);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::Dir(ref path)))
+                     if base_path == Path::new("/111") &&
+                     path == Path::new("/a")),
+            "Was: {:?}", entry);
+    let ref entry = entries[1];
+    assert!(matches!(entry, &(base_path, Ok(Entry::Dir(ref path)))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/a")),
+            "Was: {:?}", entry);
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 2);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::File{ref path, size, ..}))
+                     if base_path == Path::new("/111") &&
+                     path == Path::new("/a/.hidden") && size == 28394),
+            "Was: {:?}", entry);
+    let ref entry = entries[1];
+    assert!(matches!(entry, &(base_path, Ok(Entry::File{ref path, size, ..}))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/a/.hidden") && size == 28394),
+            "Was: {:?}", entry);
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 1);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::Dir(ref path)))
+                     if base_path == Path::new("/111") &&
+                     path == Path::new("/b")),
+            "Was: {:?}", entry);
+
+    println!("Advancing to -> /c");
+    let entries = merged_iter.advance(&EntryKind::Dir("/c"));
+    assert_eq!(entries.len(), 1);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::Dir(ref path)))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/c")),
+            "Was: {:?}", entry);
+
+    let entries = merged_iter.next().unwrap();
+    assert_eq!(entries.len(), 1);
+    let ref entry = entries[0];
+    assert!(matches!(entry, &(base_path, Ok(Entry::File{ref path, size, ..}))
+                     if base_path == Path::new("/222") &&
+                     path == Path::new("/c/empty.txt") && size == 0),
+            "Was: {:?}", entry);
+
+    assert!(merged_iter.next().is_none());
+}
